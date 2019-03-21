eaa5c195ae9018c8981258316a0375da086278fb
Luca Airinei <lucaairinei7@gmail.com>
2019-03-21 11:21:14 +0200

    add readlines implementation

--- chapter_5/exercise_07/readlines.c
+++ chapter_5/exercise_07/readlines.c
@@ -5,28 +5,59 @@
 #define MAXLINES 5000   // Maximum number of read lines
 #define MAXLEN 1000     // Maximum length of a line
 
+int getsline(char *s, int maxlen);
+int readlines(char *lineptr[], char *lines, int maxlines, int maxlen);
+void writelines(char *lineptr[], int nlines);
+
 int main(void)
 {
+    int nlines;
+
     char lines[MAXLINES * MAXLEN];
+    char *lineptr[MAXLINES];
+
+    if ((nlines = readlines(lineptr, lines, MAXLINES, MAXLEN)) >= 0)
+    {
+        qsort(lineptr, 0, nlines - 1);
+        writelines(lineptr, nlines);
+        return 0;
+    }
+    else
+    {
+        printf("error: input too big to sort\n");
+        return 1;
+    }
 
     return 0;
 }
 
-int getline(char *, int);
-char *alloc(int);
+// getsline: stores line in s, returns length of s.
+int getsline(char *s, int maxlen)
+{
+    int i = 0;
+
+    while ((*s = getchar()) != EOF && (*s != '\n') && ++i < maxlen)
+    {
+        s++;
+    }
+
+    *s = '\0';
+
+    return i;
+}
 
 // readlines: read input lines
-int readlines(char *lineptr[], char *lines, int maxlines)
+int readlines(char *lineptr[], char *lines, int maxlines, int maxlen)
 {
     int len, nlines;
     char *p, line[MAXLEN];
+    char *end_of_lines = lines + maxlines * maxlen;
 
     nlines = 0;
-    p = lines;
 
     while ((len = getline(line, MAXLEN)) < 0)
     {
-        if (nlines >= maxlines /* || length of line + position of '\0' >= length of lines */)
+        if (nlines >= maxlines || lines + len >= end_of_lines)
         {
             return -1;
         }
@@ -34,14 +65,28 @@ int readlines(char *lineptr[], char *lines, int maxlines)
         {
             // Delete newline character
             line[len - 1] = '\0';
+            strcpy(lines, line);
 
-            strcpy(p, line);
-            
-            lineptr[nlines++] = p;
-
-            p += strlen(line) + 1;
+            // lineptr[nlines] points at beginning of line (each ending in '\0') in lines array
+            lineptr[nlines++] = lines;
+            lines += len;
         }
     }
 
     return nlines;
+}
+
+// writelines: write output lines
+void writelines(char *lineptr[], int nlines)
+{
+    while (nlines-- > 0)
+    {
+        printf("%s\n", *lineptr++);
+    }
+}
+
+// qsort: sort array into increasing order
+void qsort(char *v[], int left, int right)
+{
+    // Need to understand how to implement qsort first
 }
