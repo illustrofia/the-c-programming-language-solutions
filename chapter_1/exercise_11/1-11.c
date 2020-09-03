383b2bb814bfe1bbca0cb77e58dcffb1d39ee4f6
Luca Airinei <illustrofia@gmail.com>
2020-09-03 06:07:55 +0300

    correct logical errors; update formatting; clarify statements; add observations

--- chapter_1/exercise_11/1-11.c
+++ chapter_1/exercise_11/1-11.c
@@ -7,17 +7,21 @@
 
 int main(void)
 {
-    int c, nl, nw, nc, state;
+    int c;           // Character input variable
+    int nl = 1;      // Line counter; initialized 1, because input is at least one line
+    int nw = 0;      // Word counter
+    int nc = 0;      // Character counter
+    int state = OUT; // Stores whether INside or OUTside a word; initialized OUTside of word
 
-    state = OUT;
-    nl = nw = nc = 0;
     while ((c = getchar()) != EOF)
     {
         nc++;
-        if (c == '\n') {
+
+        if (c == '\n')
+        {
             nl++;
         }
-        
+
         if (c == ' ' || c == '\n' || c == '\t')
         {
             state = OUT;
@@ -29,7 +33,7 @@ int main(void)
         }
     }
 
-    printf("%d lines, %d words, %d characters\n", nl, nw, nc);
+    printf("\n%d lines, %d words, %d characters\n", nl, nw, nc);
 }
 
 // Observations
@@ -38,3 +42,7 @@ int main(void)
 // extremely long words, tabs, extremely long
 // groups of blanks and special characters, such as
 // {}, /, ;, etc.
+
+// At the same time, I would pass on just an EOF character,
+// or a couple of newline characters to see if the program
+// 'thinks' logically.
