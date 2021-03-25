43c59aea4313903889603900fe81340025da7ca0
Luca Airinei <illustrofia@gmail.com>
2021-03-25 12:43:24 +0200

    Updated exercises.

--- chapter_1/exercise_14/1-14.c
+++ chapter_1/exercise_14/1-14.c
@@ -2,20 +2,19 @@
 
 #include <stdio.h>
 
-// The limit for the array is 255, since the ascii table has only 256 values
 #define LIMIT 255
 
 int main(void)
 {
     int chars[LIMIT];
-    
-    for (int i = 0; i < LIMIT; i++)
+
+    for (int i = 0; i <= LIMIT; i++)
     {
         chars[i] = 0;
     }
 
-    int c;          // Gets characters from input
-    int max = 0;    // Stores maximum frequency
+    int c;       // Gets characters from input
+    int max = 0; // Stores maximum frequency
 
     while ((c = getchar()) != EOF)
     {
@@ -25,21 +24,22 @@ int main(void)
             max = chars[c];
         }
     }
-    
+
     // Histogram printing explained after end of main()
     printf("\nHistogram of the characters:\n\n");
     for (int i = max; i > 0; i--)
     {
         for (int j = 0; j <= LIMIT; j++)
         {
-            if (chars[j]) {
+            if (chars[j])
+            {
                 if (chars[j] >= i)
                 {
-                    printf("###");
+                    printf(" ### ");
                 }
                 else
                 {
-                    printf("   ");
+                    printf("     ");
                 }
             }
         }
@@ -50,15 +50,50 @@ int main(void)
     // Prints bottom row of characters
     for (int i = 0; i <= LIMIT; i++)
     {
-        printf(" %c  ", i);
+        if (chars[i])
+        {
+            if (i == '\n')
+            {
+                printf("  \\n ");
+            }
+            else if (i == '\t')
+            {
+                printf("  \\t ");
+            }
+            else
+            {
+                printf("  %c  ", i);
+            }
+        }
     }
-    
+
+    printf("\n");
+
+    // Prints row of character frequencies
+    for (int i = 0; i <= LIMIT; i++)
+    {
+        if (chars[i])
+        {
+            if (chars[i] < 10)
+            {
+                printf(" 00%i ", chars[i]);
+            }
+            else if (chars[i] < 100)
+            {
+                printf(" 0%i ", chars[i]);
+            }
+            else
+            {
+                printf(" %i ", chars[i]);
+            }
+        }
+    }
+
     printf("\n");
 }
 
-
 // The for loop starts with i representing
-// the maximum character frequency. 
+// the maximum character frequency.
 // Only the most frequent characters will
 // print a symbol here. The others must print
 // blank spaces here, though, so they fill
