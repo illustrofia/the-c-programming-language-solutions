70837ab5a0a57b848faea08ba25437b5ff0df2c7
Luca Airinei <illustrofia@gmail.com>
2021-03-25 12:49:20 +0200

    Update exercises.

--- chapter_1/exercise_15/1-15.c
+++ chapter_1/exercise_15/1-15.c
@@ -11,26 +11,24 @@ int main(void)
     float fahr, celsius;
     int lower, upper, step;
 
-    lower = 0;      // lower temperature limit
-    upper = 300;    // upper temperature limit
-    step = 20;      // step size
+    lower = 0;   // lower temperature limit
+    upper = 300; // upper temperature limit
+    step = 20;   // step size
 
     fahr = lower;
-    
-    
+
     printf("Fahrenheit \t Celsius\n");
     while (fahr <= upper)
     {
         printf("%3.0f \t\t %6.1f\n", fahr, fahr_to_celsius(fahr));
         fahr += step;
     }
-    
+
     return 0;
 }
 
 // Returns celsius equivalent of fahr
 float fahr_to_celsius(float fahr)
 {
-    float celsius = (5.0 / 9.0) * (fahr - 32);
-    return celsius;
+    return (5.0 / 9.0) * (fahr - 32);
 }
