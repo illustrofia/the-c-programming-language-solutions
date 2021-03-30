3843b01b5e0d07d4c69d91c3bc03dd768974feab
Luca Airinei <illustrofia@gmail.com>
2021-03-30 16:01:05 +0300

    Update exercise.

--- chapter_1/exercise_24/error_checker.c
+++ chapter_1/exercise_24/error_checker.c
@@ -2,201 +2,55 @@
 
 #include <stdio.h>
 
-#define IN 1
-#define OUT 0
-#define INQUOTE -1
-#define MAXLINE 1000        // Maximum input line size
+#define TRUE 1
+#define FALSE 0
+#define MAXLINE 10000
 
-int getsline(char line[], int limit);
-void copy(char from[], char to[]);
+void get_program(char program[], int limit);
+void error_check(char program[]);
 
 int main(void)
 {
-    char line[MAXLINE];     // Input line
-    int length;             // Line length
-    int line_counter;
+    char program[MAXLINE];
 
-    int state;
-    int quote_start;
-    int comment_start;
-
-    int parantheses[10];
-    int p;
-    int brackets[10];
-    int brk;
-    int braces[10];
-    int brc;
-    int squotes[10];
-    int sq;
-    int dquotes[10];
-    int dq;
-    int comments;
-
-    p = 0;
-    brk = 0;
-    brc = 0;
-    sq = 0;
-    dq = 0;
-    comments = 0;
-
-    for (int i = 0; i < 10; i++)
-    {
-        parantheses[i] = 0;
-        brackets[i] = 0;
-        braces[i] = 0;
-        squotes[i] = 0;
-        dquotes[i] = 0;
-    }
-
-    state = 0;
-    line_counter = 0;
-    while ((length = getsline(line, MAXLINE)) > 0)
-    {
-        line_counter++;
-        for (int i = 0; i < length; i++)
-        {
-            if (state == OUT)
-            {
-                if ((line[i] == '/' && line[i + 1] == '/') || (line[i] == '/' && line[i + 1] == '*'))
-                {
-                    state = IN;
-                    comment_start = i;
-
-                    comments = line_counter;
-                }
-                else if (line[i] == '"' || line[i] == '\'')
-                {
-                    state = INQUOTE;
-                    quote_start = i;
-
-                    if (line[i] == '"')
-                    {
-                        dquotes[dq] = line_counter;
-                        dq++;
-                    }
-                    else if (line[i] == '\'')
-                    {
-                        squotes[sq] = line_counter;
-                        sq++;
-                    }
-                }
-                else if (line[i] == '{')
-                {
-                    braces[brc] = line_counter;
-                    brc++;
-                }
-                else if (line[i] == '}')
-                {
-                    brc--;
-                    braces[brc] = 0;
-                }
-                else if (line[i] == '[')
-                {
-                    brackets[brk] = line_counter;
-                    brk++;
-                }
-                else if (line[i] == ']')
-                {
-                    brk--;
-                    brackets[brk] = 0;
-                }
-                else if (line[i] == '(')
-                {
-                    parantheses[p] = line_counter;
-                    p++;
-                }
-                else if (line[i] == ')')
-                {
-                    p--;
-                    parantheses[p] = 0;
-                }
-            }
-            else if (state == INQUOTE && line[i] == line[quote_start] && (line[i - 1] != '\\' || line[i - 2] == '\\'))
-            {
-                state = OUT;
-
-                if (line[i] == '"')
-                {
-                    dq--;
-                    dquotes[dq] = 0;
-                }
-                else if (line[i] == '\'')
-                {
-                    sq--;
-                    squotes[sq] = 0;
-                }
-            }
-            else if (state == IN)
-            {
-                if (line[i] == '\n' && comment_start >= 0 && line[comment_start + 1] == '/')
-                {
-                    state = OUT;
-                    
-                    comments = 0;
-                }
-                else if ((line[i] == '*' && line[i + 1] == '/') && comment_start == -1)
-                {
-                    state = OUT;
-                    
-                    comments = 0;
-                }
-                else if (line[i] == '\n' && comment_start >= 0 && line[comment_start + 1] == '*')
-                {
-                    comment_start = -1;
-                }
-            }
-        }
-    }
-
-    if (comments)
-    {
-        printf("Unclosed comment starting at line %i\n\n", comments);
-    }
-
-    // Prints error messages
-    for (int i = 0; i < 10; i++)
-    {
-        if (squotes[i])
-        {
-            printf("Unclosed single quote starting at line %i\n\n", squotes[i]);
-        }
-        if (dquotes[i])
-        {
-            printf("Unclosed double quote starting at line %i\n\n", dquotes[i]);
-        }
-        if (parantheses[i])
-        {
-            printf("Unclosed parantheses starting at line %i\n\n", parantheses[i]);
-        }
-        if (brackets[i])
-        {
-            printf("Unclosed brackets starting at line %i\n\n", brackets[i]);
-        }
-        if (braces[i])
-        {
-            printf("Unclosed curly braces starting at line %i\n\n", braces[i]);
-        }
-    }
-
-    return 0;
+    get_program(program, MAXLINE);
+    error_check(program);
 }
 
-// Reads a line into s, returns length
-int getsline(char s[], int limit)
+// Gets a C program in program[]
+void get_program(char program[], int limit)
 {
-    int c, i;
+    int c, i = 0;
 
-    for (i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+    while (i < limit - 1 && (c = getchar()) != EOF)
     {
-        s[i] = c;
+        program[i++] = c;
     }
 
-    if (c == '\n')
-    {
-        s[i] = c;
-        i++;
-    }
+    program[i++] = '\n';
+    program[i] = '\0';
+}
 
-    s[i] = '\0';
-    return i;
+// Checks a C program for rudimentary syntax errors
+void error_check(char program[])
+{
+    int single_quotes = 0;
+    int double_quotes = 0;
+
+    int line_comment = FALSE;
+    int block_comment = FALSE;
+
+    int parantheses = 0;
+    int brackets = 0;
+    int braces = 0;
+
+    int i = 0;
+    while (program[i] != '\0')
+    {
+        // Check if we're inside quote
+        if (!single_quotes && program[i] == "'")
+        {
+            single_quotes++;
+        }
+    }
 }
