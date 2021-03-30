851478842eba7d6683f84d77e7e6c80aad0d0bc5
Luca Airinei <illustrofia@gmail.com>
2021-03-30 14:34:07 +0300

    Update exercise.

--- chapter_1/exercise_23/remove_comments.c
+++ chapter_1/exercise_23/remove_comments.c
@@ -2,114 +2,94 @@
 
 #include <stdio.h>
 
-#define IN 1
-#define OUT 0
-#define INQUOTE -1
-#define MAXLINE 1000        // Maximum input line size
+#define TRUE 1
+#define FALSE 0
+#define MAXLINE 10000
 
-int get_program(char line[], int limit);
-void copy(char from[], char to[]);
+void get_program(char line[], int limit);
+void remove_comments(char str[], char str_no_com[]);
 
-// Needs comments. Needs formating on that long if condition (line 73, it has a humongous amount of columns)
 int main(void)
 {
-    int length;             // Line length
-    char line[MAXLINE];     // Input line
+    char program[MAXLINE];
+    char program_no_com[MAXLINE];
 
-    int state = OUT;
+    get_program(program, MAXLINE);
+    remove_comments(program, program_no_com);
 
-    int comment_start;
-    int comment_end;
-    int quote_start;
-    // int useless line;
-
-    while ((length = getsline(line, MAXLINE)) > 0)
-    {
-        // useless_line = 0;
-        for (int i = 0; i < length; i++)
-        {
-            if (state == OUT && (line[i] == '"' || line[i] == '\''))
-            {
-                state = INQUOTE;
-                quote_start = i;
-            }
-            else if (state == INQUOTE && line[i] == line[quote_start])
-            {
-                state = OUT;
-            }
-            else if (state == OUT)
-            {
-                if ((line[i] == '/' && line[i + 1] == '/') || (line[i] == '/' && line[i + 1] == '*'))
-                {
-                    state = IN;
-                    comment_start = i;
-                }
-            }
-            else if (state == IN) // This structure needs a lot of commenting, at least for myself
-            {
-                if (line[i] == '\n' && line[comment_start + 1] == '/')
-                {
-                    line[comment_start] = '\n';
-                    line[comment_start + 1] = '\0';
-                    state = OUT;
-                    // if (comment_start == 0)
-                    // {
-                    //     useless_line++;
-                    // }
-                }
-                else if (line[i] == '\n' && line[comment_start + 1] == '*')
-                {
-                    line[comment_start] = '\n';
-                    line[comment_start + 1] = '\0';
-                    comment_start = 0;
-                    // useless_line++;
-                }
-                else if (line[i] == '\n' && comment_start == 0)
-                {
-                    line[comment_start] = '\n';
-                    line[comment_start + 1] = '\0';
-                    // useless_line++;
-                }
-                else if ((line[i] == '*' && line[i + 1] == '/') && !(line[comment_start] == '/' && line[comment_start + 1] == '/'))
-                {
-                    comment_end = i + 2;
-                    for (int j = comment_end; j <= length; j++)
-                    {
-                        line[comment_start + j - comment_end] = line[j];
-                    }
-                    state = OUT;
-                    length = length - comment_end + comment_start;
-                    i = comment_start;
-                }
-            }
-        }
-
-        // if(!useless_line)
-        // {
-            printf("%s", line);
-        // }
-    }
-
-    return 0;
+    printf("%s", program_no_com);
 }
 
-
-// Reads a line into s, returns length
-int getsline(char s[], int limit)
+void get_program(char line[], int limit)
 {
-    int c, i;
+    int c, i = 0;
 
-    for (i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+    while (i < limit - 1 && (c = getchar()) != EOF)
     {
-        s[i] = c;
+        line[i++] = c;
     }
 
-    if (c == '\n')
-    {
-        s[i] = c;
-        i++;
-    }
+    line[i] = '\0';
+}
 
-    s[i] = '\0';
-    return i;
+void remove_comments(char str[], char str_no_com[])
+{
+    int inline_com = FALSE;
+    int block_com = FALSE;
+    int in_quote = FALSE;
+
+    int i = 0, j = 0;
+    while (str[i] != '\0')
+    {
+        // If we're inside a quote, copy character
+        if (!in_quote && str[i] == '"')
+        {
+            in_quote = TRUE;
+            str_no_com[j++] = str[i++];
+        }
+        else if (in_quote && str[i] == '"')
+        {
+            in_quote = FALSE;
+            str_no_com[j++] = str[i++];
+        }
+
+        // If we're not inside quote, check if we're not inside a comment
+        if (!in_quote)
+        {
+            if (!block_com && str[i] == '/' && str[i + 1] == '*' && !inline_com)
+            {
+                block_com = TRUE;
+            }
+
+            if (str[i] == '*' && str[i + 1] == '/')
+            {
+                block_com = FALSE;
+                i += 2;
+            }
+
+            if (str[i] == '/' && str[i + 1] == '/')
+            {
+                inline_com = TRUE;
+            }
+
+            if (str[i] == '\n')
+            {
+                inline_com = FALSE;
+            }
+
+            // If we're not inside a comment, copy character
+            if (!inline_com && !block_com)
+            {
+                str_no_com[j++] = str[i++];
+            }
+            else
+            {
+                i++;
+            }
+        }
+        else // If we're not inside a quote or a comment, copy character
+        {
+            str_no_com[j++] = str[i++];
+        }
+    }
 }
