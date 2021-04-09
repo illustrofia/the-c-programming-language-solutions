4bf2638146468ee420e7d0de27354553c16351ff
Luca Airinei <illustrofia@gmail.com>
2021-04-09 23:47:03 +0300

    Update exercise.

--- chapter_2/exercise_03/hex_to_int.c
+++ chapter_2/exercise_03/hex_to_int.c
@@ -6,38 +6,45 @@
 
 int alternate_htoi(char hex[]);
 
-
 int main(void)
 {
-    char hex[MAXHEX] = "0Xf23";
+  char hex[MAXHEX] = "0Xf23";
 
-    printf("%s\n%i\n", hex, alternate_htoi(hex));
+  printf("%s\n%i\n", hex, alternate_htoi(hex));
 
-    return 0;
+  return 0;
 }
 
 // Converts string of hexadecimal number into equivalent integer value
 int alternate_htoi(char s[])
 {
-    int n = 0;
+  int n = 0, i;
 
-    int limit = (s[0] == '0') ? 2 : 0;
+  // Skip the first two characters if string starts with 0x or 0X
+  int limit = (s[0] == '0') ? 2 : 0;
 
-    for (int i = strlen(s) - 1; i >= limit; i--)
+  int s_length = strlen(s) - 1;
+
+  for (i = s_length; i >= limit; i--)
+  {
+    if (s[i] >= '0' && s[i] <= '9')
     {
-        if (s[i] <= '9')
-        {
-            n += pow(16, strlen(s) - 1 - i) * (s[i] - '0');
-        }
-        else if (s[i] <= 'F')
-        {
-            n += pow(16, strlen(s) - 1 - i) * (s[i] - 'A' + 10);
-        }
-        else if (s[i] <= 'f')
-        {
-            n += pow(16, strlen(s) - 1 - i) * (s[i] - 'a' + 10);
-        }
+      n += pow(16, s_length - i) * (s[i] - '0');
     }
+    else if (s[i] >= 'A' && s[i] <= 'F')
+    {
+      n += pow(16, s_length - i) * (s[i] - 'A' + 10);
+    }
+    else if (s[i] >= 'a' && s[i] <= 'f')
+    {
+      n += pow(16, s_length - i) * (s[i] - 'a' + 10);
+    }
+    else
+    {
+      printf("Error: invalid input.\n");
+      return 1;
+    }
+  }
 
-    return n;
+  return n;
 }
