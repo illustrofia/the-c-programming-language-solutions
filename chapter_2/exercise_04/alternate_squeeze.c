ad6fe8de723d462560620193cdd9ea26acc4e4cb
Luca Airinei <illustrofia@gmail.com>
2021-04-10 10:12:35 +0300

    Update exercise.

--- chapter_2/exercise_04/alternate_squeeze.c
+++ chapter_2/exercise_04/alternate_squeeze.c
@@ -6,30 +6,30 @@ void alternate_squeeze(char s1[], char s2[]);
 
 int main(void)
 {
-    char s1[MAXLINE] = "I love chocolate";
-    char s2[MAXLINE] = "oh, daughter";
+  char s1[MAXLINE] = "I love chocolate";
+  char s2[MAXLINE] = "oh, daughter";
 
-    alternate_squeeze(s1, s2);
+  alternate_squeeze(s1, s2);
 
-    printf("%s\n", s1);
+  printf("%s\n", s1);
 
-    return 0;
+  return 0;
 }
 
 void alternate_squeeze(char s1[], char s2[])
 {
-    int i, j, k;
+  int i, j, k;
 
-    for (k = 0; s2[k] != '\0'; k++)
+  for (k = 0; s2[k] != '\0'; k++)
+  {
+    for (i = j = 0; s1[i] != '\0'; i++)
     {
-        for (i = j = 0; s1[i] != '\0'; i++)
-        {
-            if (s1[i] != s2[k])
-            {
-                s1[j++] = s1[i];
-            }
-        }
-
-        s1[j] = '\0';
+      if (s1[i] != s2[k])
+      {
+        s1[j++] = s1[i];
+      }
     }
+
+    s1[j] = '\0';
+  }
 }
