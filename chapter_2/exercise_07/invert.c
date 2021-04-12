1e3310c4d047f65beb2c122379adab7f09c3e002
Luca Airinei <illustrofia@gmail.com>
2021-04-12 12:15:09 +0300

    Update exercise.

--- chapter_2/exercise_07/invert.c
+++ chapter_2/exercise_07/invert.c
@@ -1,24 +1,28 @@
-// Birth of invert()
-
 #include <stdio.h>
 
 int invert(int x, int p, int n);
 
 int main(void)
 {
-    int x = 85;
-    
-    printf("%i\n", invert(x, 3, 2));
+  int x = 85;
+  int p = 3;
+  int n = 2;
 
-    return 0;
+  printf("%i\n", invert(x, p, n));
+
+  return 0;
 }
-
 
 // Returns x with the n bits that begin at position p inverted
 int invert(int x, int p, int n)
 {
-    int ones_with_inverted_n_bits_of_x_in_position_p = ~((~(~0 << n) << (p + 1 - n)) & x);
-    int x_with_n_bits_in_position_p_equal_to_1 = (~(~0 << n) << (p + 1 - n)) | x;
+  // Example with p = 3, n = 2
+  // Creates xxxx 00xx
+  int scooped_x = ~(~(~0 << n) << p + 1 - n) & x;
 
-    return ones_with_inverted_n_bits_of_x_in_position_p & x_with_n_bits_in_position_p_equal_to_1;
+  // Creates 0000 ii00
+  int inverted_x_bits = (~(~0 << n) << p + 1 - n) & ~x;
+
+  // xxxx 00xx | 0000 ii00 == xxxx iixx
+  return scooped_x | inverted_x_bits;
 }
