a2969fd772fc7904798043e0bbe45a0ef4a4a81e
Luca Airinei <illustrofia@gmail.com>
2021-04-13 10:54:44 +0300

    Update exercise.

--- chapter_2/exercise_07/invert.c
+++ chapter_2/exercise_07/invert.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
+#include <math.h>
 
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c\n"
-#define BYTE_TO_BINARY(byte)   \
-  (byte & 128 ? '1' : '0'),    \
-      (byte & 64 ? '1' : '0'), \
-      (byte & 32 ? '1' : '0'), \
-      (byte & 16 ? '1' : '0'), \
-      (byte & 8 ? '1' : '0'),  \
-      (byte & 4 ? '1' : '0'),  \
-      (byte & 2 ? '1' : '0'),  \
-      (byte & 1 ? '1' : '0')
-
+void printbin(unsigned int x);
 int invert(int x, int p, int n);
 
 int main(void)
 {
   int x = 0b01010101;
   int p = 3;
-  int n = 2;
+  int n = 4;
 
-  printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(x));
-  printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(invert(x, p, n)));
+  printbin(x);
+  printbin(invert(x, p, n));
 
   return 0;
+}
+
+void printbin(unsigned int x)
+{
+  int n = sizeof(unsigned int);
+
+  printf("0b");
+
+  int i;
+  for (i = n * 8 - 1; i >= 0; i--)
+  {
+    (x & (unsigned int)pow(2, i)) ? putchar('1') : putchar('0');
+  }
+
+  putchar('\n');
 }
 
 // Returns x with the n bits that begin at position p inverted
@@ -30,10 +36,10 @@ int invert(int x, int p, int n)
 {
   // Example with p = 3, n = 2
   // Creates xxxx 00xx
-  int scooped_x = ~(~(~0 << n) << p + 1 - n) & x;
+  int scooped_x = ~(~(~0 << n) << (p + 1 - n)) & x;
 
   // Creates 0000 ii00
-  int inverted_x_bits = (~(~0 << n) << p + 1 - n) & ~x;
+  int inverted_x_bits = (~(~0 << n) << (p + 1 - n)) & ~x;
 
   // xxxx 00xx | 0000 ii00 == xxxx iixx
   return scooped_x | inverted_x_bits;
