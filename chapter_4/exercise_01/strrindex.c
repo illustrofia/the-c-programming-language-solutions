fa884ab1ed3b7b4f2081383bbc6970a3517d8b7a
Luca Airinei <illustrofia@gmail.com>
2021-04-16 13:59:28 +0300

    Update exercise.

--- chapter_4/exercise_01/strrindex.c
+++ chapter_4/exercise_01/strrindex.c
@@ -1,5 +1,3 @@
-// Birth of strrindex()
-
 #include <stdio.h>
 #include <string.h>
 
@@ -12,51 +10,51 @@ char pattern[] = "ould";
 
 int main(void)
 {
-    char s[MAXLEN];
+  char s[MAXLEN];
 
-    while (getsline(s, MAXLEN) > 0)
+  while (getsline(s, MAXLEN) > 0)
+  {
+    if (strrindex(s, pattern))
     {
-        if (strrindex(s, pattern))
-        {
-            printf("rightmost position of t in s is %d\n", strrindex(s, pattern));
-        }
+      printf("%d\n", strrindex(s, pattern));
     }
+  }
 }
 
 int getsline(char s[], int lim)
 {
-    int c, i;
+  int c, i;
 
-    i = 0;
-    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
-    {
-        s[i++] = c;
-    }
+  i = 0;
+  while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+  {
+    s[i++] = c;
+  }
 
-    if (c == '\n')
-    {
-        s[i++] = c;
-    }
-    
-    s[i] = '\0';
+  if (c == '\n')
+  {
+    s[i++] = c;
+  }
 
-    return i;
+  s[i] = '\0';
+
+  return i;
 }
 
 int strrindex(char s[], char t[])
 {
-    int i, j, k;
+  int i, j, k;
 
-    for (int i = strlen(s) - 1; i >= strlen(t) - 1; i--)
+  for (int i = strlen(s) - 1; i >= strlen(t) - 1; i--)
+  {
+    for (j = i, k = strlen(t) - 1; j >= 0 && k >= 0 && s[j] == t[k]; j--, k--)
+      ;
+
+    if (k < 0)
     {
-        for (j = i, k = strlen(t) - 1; j >= 0 && k >= 0 && s[j] == t[k]; j--, k--)
-            ;
-        
-        if (++k == 0)
-        {
-            return i - strlen(t) + 1;
-        }
+      return i - strlen(t) + 1;
     }
+  }
 
-    return -1;
+  return -1;
 }
