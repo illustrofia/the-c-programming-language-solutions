9e5dfeea5be0f41ae3c8b7736410c9d08449f019
Luca Airinei <illustrofia@gmail.com>
2021-04-21 11:40:33 +0300

    Update exercise.

--- chapter_4/exercise_12/recursive_itoa.c
+++ chapter_4/exercise_12/recursive_itoa.c
@@ -1,52 +1,33 @@
-// Description
-
 #include <stdio.h>
 #include <string.h>
 
 #define MAXLEN 100
 
-void recursive_itoa(int n, char s[]);
+void itoa_rec(int n, char s[]);
 void reverse(char s[]);
 
 int main(void)
 {
-  char s[MAXLEN] = "";
   int n = 12345;
+  char s[MAXLEN];
 
-  recursive_itoa(n, s);
+  itoa_rec(n, s);
 
-  printf("%i = '%s'\n", n, s);
+  printf("%i = \"%s\"\n", n, s);
 
   return 0;
 }
 
-void recursive_itoa(int n, char s[])
+void itoa_rec(int n, char s[])
 {
-  int s_len = strlen(s);
-
-  s[s_len] = n % 10 + '0';
-
   if (n / 10)
   {
-    return recursive_itoa(n / 10, s);
+    itoa_rec(n / 10, s);
   }
 
-  s[++s_len] = '\0';
+  s[strlen(s)] = n % 10 + '0';
 
-  reverse(s);
+  s[strlen(s)] = '\0';
 
   return;
-}
-
-void reverse(char s[])
-{
-  int i = strlen(s) - 1;
-  int aux;
-
-  for (int j = 0; j < i; i--, j++)
-  {
-    aux = s[i];
-    s[i] = s[j];
-    s[j] = aux;
-  }
 }
