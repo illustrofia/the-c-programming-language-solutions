bbc23ebee1bb1b7d6e7fd4a59ca79eb034a51852
Luca Airinei <illustrofia@gmail.com>
2021-05-12 11:46:08 +0300

    Made progress on exercise.

--- chapter_5/exercise_11/entab.c
+++ chapter_5/exercise_11/entab.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TAB 8
 
-int main(void)
+int main(int argc, char *argv[])
 {
   int c;
   int spaces;
@@ -44,6 +45,56 @@ int main(void)
 
 /**
  *
+ * argc: number of arguments. argv[argc] == '\0
+ * argv: [prog_title] 4 8 16
  *
- *
-**/
+ **/
+
+int main2(int argc, char *argv[])
+{
+  int c;
+  int spaces = 0;
+  int line_position = 0;
+  int i = 1;
+  int current_tab_stop = atoi(*(argv + 1));
+
+  while ((c = getchar()) != EOF)
+  {
+    line_position++;
+
+    for (int i = argc - 1; i >= 1; i--)
+    {
+      if (line_position > atoi(*(argv + i)))
+      {
+        current_tab_stop = atoi(*(argv + i));
+        break;
+      }
+    }
+
+    if (c == ' ')
+    {
+      spaces++;
+
+      if (line_position % current_tab_stop == 0 && spaces > 0)
+      {
+        putchar('\t');
+        spaces = 0;
+      }
+    }
+    else
+    {
+      while (spaces)
+      {
+        puchar(' ');
+        spaces--;
+      }
+
+      putchar(c);
+    }
+
+    if (c == '\n')
+    {
+      line_position = 0;
+    }
+  }
+}
